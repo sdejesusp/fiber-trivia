9045f837bee80b0b8faa5e0fe63da4c6f27813a7
Sterling De jesus <s.dejesusp@gmail.com>
2024-03-16 12:33:52 -0400

    postgres db setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sdejesusp/fiber-trivia/database"
+)
 
 func main() {
+    database.ConnectDb()
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, from a Docker fiber container")
+        return c.SendString("Hello, from a Docker fiber container.")
     })
 
     app.Listen(":3000")
